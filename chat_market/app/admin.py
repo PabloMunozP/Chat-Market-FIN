d9cf6c1babc138182f15d7fa0bf6cccb5caf16b3
Pablo Muñoz Poblete <pablo.munoz2@mail.udp.cl>
2022-05-28 23:39:04 -0400

    eliminar carpeta sobrante

--- chat_market/app/admin.py
+++
@@ -1,7 +0,0 @@
-from curses import tigetflag
-from django.contrib import admin
-from .models import Usuario,Ticket,Oferta
-# Register your models here.
-admin.site.register(Usuario)
-admin.site.register(Ticket)
-admin.site.register(Oferta)
